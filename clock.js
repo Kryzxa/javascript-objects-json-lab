9b0f1691641f8be6f21cc7e7dda00e671a63f420
Kryzxa <Matthewlin1243@gmail.com>
2025-11-11 13:30:23 +0000

    suh

---
+++ clock.js
@@ -0,0 +1,6 @@
+(function(){ 
+    console.info("Hello JS"); 
+    const myNode = document.createElement("div");
+    const digitalClock = document.getElementById("digitalClock")
+    digitalClock.appendChild(myNode);
+    })();
